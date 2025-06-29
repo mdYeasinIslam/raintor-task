e842909e1eae49a9588735b4cb980a833f3571e3
mdYeasinIslam <hyeasinislam@gamil.com>
2025-06-29 17:02:05 +0600

    project setup successfully

---
+++ src/font/font.tsx
@@ -0,0 +1,20 @@
+// // app/fonts/fonts.ts (you can name this file anything)
+
+// import localFont from "next/font/local";
+
+// export const sportingGrotesque = localFont({
+//   src: [
+//     {
+//       path: "/SportingGrotesque-Regular.otf",
+//       weight: "400",
+//       style: "normal",
+//     },
+//     // {
+//     //   path: "/SportingGrotesque-Bold.otf",
+//     //   weight: "700",
+//     //   style: "normal",
+//     // },
+//   ],
+//   variable: "--font-sporting",
+//   display: "swap",
+// });
